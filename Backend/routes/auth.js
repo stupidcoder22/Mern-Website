555a13d4106a4a856078497c265618b67a16161b
prateek <stupidcoder22@gmail.com>
2022-04-21 21:01:31 +0530

    jwt integration and token generation completed

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Userschema = require("../model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtKey = "secret";
 
 router.get("/", (req, res) => {
   res.send("home page router");
@@ -19,11 +22,69 @@ router.post("/register", async (req, res) => {
       return res.status(401).json({ error: "Email already exist" });
     }
 
-    const userdata = await new Userschema(req.body);
+    if (pass != repass) {
+      return res
+        .status(401)
+        .json({ error: "password and confirm password should be same" });
+    }
+
+    const secure = await bcrypt.hash(pass, 10);
+
+    const userdata = await new Userschema({
+      name,
+      email,
+      phone,
+      work,
+      pass: secure,
+      repass: secure,
+    });
     const user = await userdata.save();
-    res.send(user);
+
+    const data = {
+      id: user._id.toString(),
+    };
+    const token = jwt.sign(data, jwtKey);
+    res.send({ user: user, token: token });
   } catch (error) {
     res.status(500).send(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, pass } = req.body;
+  if (!email || !pass) {
+    return res.status(400).json({ error: "Please fill the data Properly" });
+  }
+
+  try {
+    const userdata = await Userschema.findOne({ email: email });
+    if (!userdata) {
+      return res.status(401).json({ error: "Email doesn't exist" });
+    }
+
+    const val = await bcrypt.compare(pass, userdata.pass);
+    if (val) {
+      const data = {
+        id: userdata._id.toString(),
+      };
+      const token = jwt.sign(data, jwtKey);
+      res.json({ info: "You have logged in succesfully", token: token });
+    } else {
+      res.json("wrong password");
+    }
+
+    // if (val) {
+    //   jwt.sign({ userdata }, jwtKey, (err, token) => {
+    //     if (err) {
+    //       res.send({ result: "something went wrong" });
+    //     }
+    //     res.json({ userdata: userdata, token: token });
+    //   });
+    // } else {
+    //   res.status(403).json("Wrong Password");
+    // }
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
