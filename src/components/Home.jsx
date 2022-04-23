6a4e5aa1ab9b678dbe9fa66ca7b7dcbcc17b82a6
prateek <stupidcoder22@gmail.com>
2022-04-23 08:29:14 +0530

    HOME PAGE UI COMPLETED

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-page container-fluid">
+      <div className="home-div">
+        <p className="pt-5">WELCOME GUYS</p>
+        <h1>We Are The MERN Developer</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
